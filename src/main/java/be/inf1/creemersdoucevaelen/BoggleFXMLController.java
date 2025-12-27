40e6602a86cabe9c0f2fededea3c83066f3a7370
Kirsten Doucé <Kirsten Douc@Laptop_KirstenD>
2025-12-27 15:00:05 +0100

    Tweede versie van het project

---
+++ src/main/java/be/inf1/creemersdoucevaelen/BoggleFXMLController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package be.inf1.creemersdoucevaelen;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kirsten Doucé
+ */
+public class BoggleFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module be.inf1.creemersdoucevaelen {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens be.inf1.creemersdoucevaelen to javafx.fxml;
     exports be.inf1.creemersdoucevaelen;

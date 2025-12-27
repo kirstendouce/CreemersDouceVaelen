1a84913cb62c9ef3ac0ca31101aefff1208baa72
Kirsten Doucé <Kirsten Douc@Laptop_KirstenD>
2025-12-27 17:21:57 +0100

    Dobbelsteen.java

--- src/main/java/be/inf1/creemersdoucevaelen/model/Dobbelsteen.java
+++ src/main/java/be/inf1/creemersdoucevaelen/model/Dobbelsteen.java
@@ -6,39 +6,121 @@ package be.inf1.creemersdoucevaelen.model;
 
 /**
  *
- * @author Kirsten Doucé
+ * @author Maarten Creemers, Kirsten Doucé & Manten Vaelen
  */
 public class Dobbelsteen {
-    private Boggle letter;
-
-   
+    private double nummer;
+    private String letter;
 
     /**
      * constructor voor elke dobbelsteen
      * @param letter 
      */
-    public Dobbelsteen(Boggle letter) {
-        this.letter = letter;
+    public Dobbelsteen() {
+        nummer =(Math.random()*100)/4;
+        
+        switch( (int) Math.round(nummer)) {
+            case 0: 
+                letter = "A";
+                break;
+            case 1: 
+                letter = "B";
+                break;
+            case 2: 
+                letter = "C";
+                break;
+            case 3: 
+                letter = "D";
+                break;
+            case 4: 
+                letter = "E";
+                break;
+            case 5: 
+                letter = "F";
+                break;
+            case 6: 
+                letter = "G";
+                break;
+            case 7: 
+                letter = "H";
+                break;
+            case 8: 
+                letter = "I";
+                break;
+            case 9: 
+                letter = "J";
+                break;    
+            case 10: 
+                letter = "K";
+                break;    
+            case 11: 
+                letter = "L";
+                break;
+            case 12: 
+                letter = "M";
+                break;
+            case 13: 
+                letter = "N";
+                break;
+            case 14: 
+                letter = "O";
+                break;
+            case 15: 
+                letter = "P";
+                break;
+            case 16: 
+                letter = "Q";
+                break;
+            case 17: 
+                letter = "R";
+                break;
+            case 18: 
+                letter = "S";
+                break;
+            case 19: 
+                letter = "T";
+                break;
+            case 20: 
+                letter = "U";
+                break;
+            case 21: 
+                letter = "V";
+                break;
+            case 22: 
+                letter = "W";
+                break;
+            case 23: 
+                letter = "X";
+                break;
+            case 24: 
+                letter = "Y";
+                break;
+            case 25: 
+                letter = "Z";
+                break;
+            default: 
+                letter = "A";
+        }
+        
     }
 
     /**
      * @return the letter
-     */
+     *//*
     public Boggle getLetter() {
         return letter;
-    }
+    }*/
 
     /**
      * @param letter the letter to set
      */
-    public void setLetter(Boggle letter) {
+    /*public void setLetter(Boggle letter) {
         this.letter = letter;
-    }
+    }*/
+
     
     
-    
-    
-    }
+}
     
     
     

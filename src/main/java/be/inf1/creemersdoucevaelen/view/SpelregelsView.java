6956c65c908c79bfd80652beee6d7af3277f4076
Kirsten Doucé <Kirsten Douc@Laptop_KirstenD>
2026-01-14 16:30:42 +0100

    Veel

--- src/main/java/be/inf1/creemersdoucevaelen/view/SpelregelsView.java
+++ src/main/java/be/inf1/creemersdoucevaelen/view/SpelregelsView.java
@@ -16,15 +16,12 @@ import javafx.scene.text.Text;
 public class SpelregelsView extends Region {
     
     public SpelregelsView() {
-       Rectangle regels = new Rectangle(230,410, Color.WHITE); 
+       Rectangle regels = new Rectangle(2000,1000, Color.BEIGE); 
        
              Text regel = new Text("SPELREGELS \n"
                + "\n1) Elk woord moet minstens 3 letters lang zijn."
-               + "\n2) Je kan woorden enkel vormen door "
-               + "\nhorizontaal,verticaal en diagonaal "
-               + "\naangrenzende letters aan te klikken. "
-               + "\n3) Je kan elk woord maar 1 keer "
-               + "\nopgeven voor punten."
+               + "\n2) Je kan woorden enkel vormen door horizontaal,verticaal en diagonaal aangrenzende letters aan te klikken. "
+               + "\n3) Je kan elk woord maar 1 keer opgeven voor punten."
                + "\n4) De puntentelling is als volgt: "
                + "\n3 of 4 letters = 1 punt "
                + "\n5 letters = 2 punten "
@@ -32,7 +29,7 @@ public class SpelregelsView extends Region {
                + "\n7 letters = 5 punten "
                + "\n8 of meer letters = 11 punten");
        regel.setY(30);
-       regel.setX(30);
+       regel.setX(10);
        getChildren().addAll(regels, regel);
        
        //De regels enkel in het kader;
